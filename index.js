3ed2d6d1856541d3ac178253bba86ad4fedc4c5d
BernardoVxexra <bernardo.santos13@etec.sp.gov.br>
2024-09-13 00:03:35 -0300

    fundamentos 001

---
+++ index.js
@@ -0,0 +1,9 @@
+// hello world
+
+//let e const qual a diferença? simples
+//let você pode mudar a qualquer hora, já const não é fixo
+
+let message = "ola mundo!"
+console.log(message);
+console.log(message);
+console.log(message);
